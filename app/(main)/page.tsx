3c70a26aa36d927274e91bae79e8c2ecfbd71df1
imadnanhassan <abulhassan.dev@gmail.com>
2025-07-11 00:16:15 +0600

    Add "use client" directive to authentication and dashboard pages for client-side rendering

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FeaturesSection } from "@/components/ui/landing/features-section";
 import { HeroSection } from "@/components/ui/landing/hero-section";
 import { StatsSection } from "@/components/ui/landing/stats-section";
